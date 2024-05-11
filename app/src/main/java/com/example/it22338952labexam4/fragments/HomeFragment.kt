7e2fda5266b827b18123fcec55b1a2b60f07377d
chala16 <chalanakaveeshami@gmail.com>
2024-05-11 23:05:15 +0530

    add fragments

--- app/src/main/java/com/example/it22338952labexam4/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/it22338952labexam4/fragments/HomeFragment.kt
@@ -3,58 +3,117 @@ package com.example.it22338952labexam4.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.it22338952labexam4.MainActivity
 import com.example.it22338952labexam4.R
+import com.example.it22338952labexam4.adapter.TodoAdapter
+import com.example.it22338952labexam4.databinding.FragmentHomeBinding
+import com.example.it22338952labexam4.model.Todo
+import com.example.it22338952labexam4.viewmodel.TodoViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextListener, MenuProvider {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var homeBinding: FragmentHomeBinding? = null
+    private val binding get() = homeBinding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var todoesViewModel: TodoViewModel
+    private lateinit var todoAdapter: TodoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        todoesViewModel = (activity as MainActivity).todoViewModel
+        setupHomeRecyclerView()
+
+        binding.addNoteFab.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_addNoteFragment)
+
+        }
+    }
+
+    private fun updateUI(todo: List<Todo>?){
+        if (todo != null){
+            if(todo.isNotEmpty()){
+                binding.emptyNotesImage.visibility = View.GONE
+                binding.homeRecyclerView.visibility = View.VISIBLE
+            } else {
+                binding.emptyNotesImage.visibility = View.VISIBLE
+                binding.homeRecyclerView.visibility = View.VISIBLE
             }
+        }
+    }
+
+    private fun setupHomeRecyclerView(){
+        todoAdapter = TodoAdapter()
+        binding.homeRecyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            setHasFixedSize(true)
+            adapter = todoAdapter
+        }
+
+        activity?.let {
+            todoesViewModel.getAllTodoes().observe(viewLifecycleOwner){ todo ->
+                todoAdapter.differ.submitList(todo)
+                updateUI(todo)
+            }
+        }
+    }
+
+    private fun searchTodo(query: String?){
+        val searchQuery = "%$query"
+
+        todoesViewModel.searchTodo(searchQuery).observe(this) {list ->
+            todoAdapter.differ.submitList(list)
+        }
+    }
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        if (newText != null){
+            searchTodo(newText)
+        }
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        homeBinding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.clear()
+        menuInflater.inflate(R.menu.home_menu, menu)
+
+        val menuSearch = menu.findItem(R.id.searchMenu).actionView as SearchView
+        menuSearch.isSubmitButtonEnabled = false
+        menuSearch.setOnQueryTextListener(this)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return false
     }
 }
